7cc5c2c61db7babf754548519f6045363861333a
OhidulAlam <mdtasib442@gmail.com>
2021-03-16 13:40:24 +0600

    functional and class component

---
+++ src/Components/ClassAndFunc/ClassComp.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ClassComp extends Component {
+    render() {
+        return <h3>I am Class Component</h3>
+    }
+}
+
+export default ClassComp;
---
+++ src/Components/ClassAndFunc/FunctionalComp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// function FunctionalComp() {
+//     return (
+//         <h2>My Name Is Mohammad Tasib</h2>
+//     )
+// }
+
+
+const FunctionalComp = () => <h2>My name is Mohammad Tasib</h2>
+
+export default FunctionalComp;
