58b05988bab2008a86544be9fc440d56939530f6
OhidulAlam <mdtasib442@gmail.com>
2021-04-08 10:43:29 +0600

    Context

--- src/Components/Context/ComponentE.js
+++ src/Components/Context/ComponentE.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import ComponentF from './ComponentF';
+import UserContext from './userContext';
 
 class ComponentE extends Component {
+    static contextType = UserContext;
+
+
     render() {
         return (
             <div>
+                <h3>Component E context --- Hey! {this.context}</h3>
                 <ComponentF />
             </div>
         );
     }
 }
+
+// ComponentE.contextType = UserContext;
 
 export default ComponentE;
--- src/Components/Context/ComponentF.js
+++ src/Components/Context/ComponentF.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { UserConsumer } from './userContext';
 
 class ComponentF extends Component {
     render() {
         return (
             <div>
-                this is ComponentF
+                <UserConsumer>
+                    {
+                        (userName) => {
+                            return <h2>Hello {userName}</h2>
+                        }
+                    }
+                </UserConsumer>
             </div>
         );
     }
