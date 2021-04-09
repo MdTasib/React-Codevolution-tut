1990e25e3678327d8c3080bccd722efd43c88ed4
MdTasib <ohidulalam442@gmail.com>
2021-04-09 22:29:23 +0600

    useEffect Hook -- fatching data

--- src/Components/Hooks/useEffect/UniqueDataLoad.js
+++ src/Components/Hooks/useEffect/UniqueDataLoad.js
@@ -4,21 +4,27 @@ import axios from 'axios';
 function UniqueDataLoad() {
     const [post, setPost] = useState({});
     const [id, setId] = useState(1);
+    const [idFormButtonClick, setIdFormButtonClick] = useState(1);
 
     useEffect(() => {
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${idFormButtonClick}`)
             .then(response => {
-                console.log(response.data)
+                console.log(response.data);
                 setPost(response.data);
             })
             .catch(error => {
                 console.log(error);
             })
-    }, [id])
+    }, [idFormButtonClick]);
+
+    const handleClick = () => {
+        setIdFormButtonClick(id);
+    }
 
     return (
         <div>
             <input type="text" value={id} onChange={e => setId(e.target.value)} />
+            <button type="submit" onClick={handleClick}>Fetch Data</button>
             <ul>
                 {post.title}
             </ul>
